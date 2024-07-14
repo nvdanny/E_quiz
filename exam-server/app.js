d21968c15f18a35beb1eb03e66b2c3337e740198
IMnhvjp <levanminh19102003@gmail.com>
2024-07-14 22:12:49 +0700

    update setup

--- exam-server/app.js
+++ exam-server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv').config;
 const bodyParser = require('body-parser');
 const config = require('./config');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const authRoutes = require('./routes/auth');
 const questionRoutes = require('./routes/questions');
@@ -22,6 +25,30 @@ app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/questions', questionRoutes);
 app.use('/api/exams', examRoutes);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "E_quiz",
+      version: "1.0.11",
+    },
+    servers: [
+      {
+        url: "",
+        descripton: "Live server"
+      },
+      {
+        url: "http://localhost:3000/api",
+        description: "Local server"
+      },
+    ],
+  },
+  apis: ["./swagger/*.yaml"],
+}
+
+const openapiSpecification = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
