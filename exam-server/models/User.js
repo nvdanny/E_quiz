3fa40e34a387b8602017b1ed4bc0f974a56eabe9
IMnhvjp <levanminh19102003@gmail.com>
2024-08-08 15:36:48 +0700

    modify user schema

--- exam-server/models/User.js
+++ exam-server/models/User.js
@@ -3,9 +3,15 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: { type: String, required: true },
+  birthday: {type: String, required: true},
   password: { type: String, required: true },
   email: {type: String, required: true, unique: true},
   phoneNumber: {type: String, required: true, unique: true},
+  university: {type: String, required: true},
+  major: {type: String, required: true},
+  year: {type: Number, required: true},
+  studentId: {type: String, required: false},
+  linkFb: {type: String, required: true},
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
   submission: { type: Schema.ObjectId, ref: "Submission" }
 });
