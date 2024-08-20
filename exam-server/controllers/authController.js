21f375dfe9134ff938af93b3d8ca39cbf16cff49
IMnhvjp <levanminh19102003@gmail.com>
2024-08-20 22:31:40 +0700

    update

--- exam-server/controllers/authController.js
+++ exam-server/controllers/authController.js
@@ -3,15 +3,18 @@
 // const jwt = require('jsonwebtoken');
 // const config = require('../config');
 const authService = require('../services/authService');
+const mailService = require('../services/mailService')
 
 module.exports = {
   signUp : async (req, res) => {
     try {
       const response  = await authService.signUp(req.body);
-      console.log(response)
       if (response.error) {
         return res.status(400).json({ msg: response.error });
       }
+      mailService.sendMail(response.data.email, mailService.registerContent).catch(err => {
+        console.log(err);
+      });
       return res.status(200).json(response);
     }
     catch(err) {
--- exam-server/services/questionService.js
+++ exam-server/services/questionService.js
@@ -2,6 +2,21 @@ const Question = require('../models/Question');
 
 module.exports = {
     createQuestion: async (data) => {
+        try {
+            const newQuestion = await Question.create({
+                description: data.description,
+                imageUrl: data.imageUrl,
+                
+            })
+        }
+        catch(err) {
+            return {
+                error: err,
+            }
+        }
+    },
+
+    getOneQuestion: async (data) => {
         
     }
 }
