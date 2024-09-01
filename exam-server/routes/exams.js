8ce6f401565bb94fa672083a3f902697bbb64321
IMnhvjp <levanminh19102003@gmail.com>
2024-09-01 21:40:38 +0700

    fix exam route

--- exam-server/routes/exams.js
+++ exam-server/routes/exams.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const examController = require("../controllers/examController");
 const { auth, admin } = require("../middlewares/auth");
 
-router.post("/", examController.createExam);
-router.get("/list", examController.getAllExam);
-router.get("/:examId", examController.getExam);
-router.put("/:examId", examController.getExam);
-router.delete("/:examId", examController.deleteExam);
+router.post("/", [auth, admin], examController.createExam);
+router.get("/list", [auth, admin], examController.getAllExam);
+router.get("/:examId", auth, examController.getExam);
+router.put("/:examId", [auth, admin], examController.getExam);
+router.delete("/:examId", [auth, admin], examController.deleteExam);
 
 module.exports = router;
