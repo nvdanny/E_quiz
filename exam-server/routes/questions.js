9f5267eb2ed9277f68285bd151be0a9cadd71b17
IMnhvjp <levanminh19102003@gmail.com>
2024-09-01 21:42:09 +0700

    fix question route

--- exam-server/routes/questions.js
+++ exam-server/routes/questions.js
@@ -5,11 +5,11 @@ const { auth, admin } = require("../middlewares/auth");
 const uploadCloud = require("../middlewares/uploader");
 
 // router.post("/create", [auth, admin, uploadCloud.single("img")], questionController.createQuestion);
-router.post("/", questionController.createQuestion);
+router.post("/", [auth, admin], questionController.createQuestion);
 router.get("/list", [auth, admin], questionController.getAllQuestion);
 router.get("/:questionId", auth, questionController.getQuestion);
 // router.put("/update/:questionId", [auth, admin, uploadCloud.single("img")], questionController.editQuestion);
-router.put("/:questionId", questionController.editQuestion);
+router.put("/:questionId", [auth, admin], questionController.editQuestion);
 router.delete("/:questionId", [auth, admin], questionController.deleteQuestion);
 
 module.exports = router;
