11f7d579766e2c4d72a8d3bb2f1cfec9b37c8bf3
Tyler Le <thanh.le@mission.plus>
2024-09-05 22:50:05 +0700

    fix: add cors

--- exam-server/app.js
+++ exam-server/app.js
@@ -11,6 +11,14 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: "https://blndt-sec-bav.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
