7eea4de287027acd84136efe5346c9f1aedd6f8f
Tyler Le <thanh.le@mission.plus>
2024-09-05 21:54:13 +0700

    add build

---
+++ frontend/build/static/js/106.3733d77f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[106],{106:(e,t,o)=>{o.r(t),o.d(t,{default:()=>c});var n=o(65043),r=o(73216),a=o(70579);const c=()=>{const e=(0,r.Zp)();return(0,n.useEffect)((()=>{localStorage.removeItem("accessToken"),localStorage.removeItem("userInfo"),localStorage.removeItem("timeLeft"),e("/login")}),[e]),(0,a.jsx)("div",{children:"\u0110ang \u0111\u0103ng xu\u1ea5t..."})}}}]);
+//# sourceMappingURL=106.3733d77f.chunk.js.map
