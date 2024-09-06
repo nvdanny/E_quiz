d0163502beb6b03c49df247892f82f9ed9deafbc
IMnhvjp <levanminh19102003@gmail.com>
2024-09-06 17:24:27 +0700

    test baseUrl

--- frontend/src/api/ApiClient.js
+++ frontend/src/api/ApiClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://api.blndt-sec-bav.com/",
+  // baseURL: "https://api.blndt-sec-bav.com/",
+  baseURL: "http://localhost:5000/",
 });
 apiClient.interceptors.response.use(
   (response) => response,
