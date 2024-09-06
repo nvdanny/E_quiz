317680b482a64f71d1b0c05baf3615f39f880abe
IMnhvjp <levanminh19102003@gmail.com>
2024-09-06 21:05:43 +0700

    change PORT

--- exam-server/app.js
+++ exam-server/app.js
@@ -9,7 +9,8 @@ const swaggerUi = require("swagger-ui-express");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 8000;
 
 app.use(cookieParser());
 app.use(bodyParser.json());
