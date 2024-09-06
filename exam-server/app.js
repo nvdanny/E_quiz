8945941d867744a538f83abe1f398560b0e93aa0
IMnhvjp <levanminh19102003@gmail.com>
2024-09-06 21:08:45 +0700

    fix

--- exam-server/app.js
+++ exam-server/app.js
@@ -9,8 +9,7 @@ const swaggerUi = require("swagger-ui-express");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-// const PORT = process.env.PORT || 5000;
-const PORT = 8000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(bodyParser.json());
