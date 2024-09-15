180db6e015c65fdbd24468b92be3c638eff53037
ducanh <anh.lehuuducanh@kool-soft.com>
2024-09-15 19:15:27 +0700

    123456

--- exam-server/models/User.js
+++ exam-server/models/User.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
   password: { type: String, required: true },
   email: {type: String, required: true, unique: true},
   phoneNumber: {type: String, required: true, unique: true},
-  region: {type: String, required: true},
+  region: {type: String, required: false},
   university: {type: String, required: true},
   major: {type: String, required: true},
   year: {type: Number, required: true},
